3c228e2f98ed031feb3070a04e54458118ee1a42
Dan Carlier <dan.carlier@gmail.com>
2023-06-10 22:32:59 +1000

    Create application (form) component

---
+++ src/components/application/application.tsx
@@ -0,0 +1,27 @@
+export const Application = () => {
+  return (
+    <form>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" />
+      </div>
+      <div>
+        <label htmlFor="job-location">Job location</label>
+        <select id="job-location">
+          <option value="">Select a country</option>
+          <option value="US">United States</option>
+          <option value="GB">United Kingdom</option>
+          <option value="CA">Canada</option>
+          <option value="IN">India</option>
+          <option value="AU">Australia</option>
+        </select>
+      </div>
+      <div>
+        <label>
+          <input type="checkbox" id="terms" /> I agree to the terms and conditions
+        </label>
+      </div>
+      <button>Submit</button>
+    </form>
+  )
+}
