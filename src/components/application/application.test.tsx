7ba5627fedb97b1437c5d0fcd0a3dce1637aa7ef
Dan Carlier <dan.carlier@gmail.com>
2023-06-15 18:05:13 +1000

    Examples of getByLabelText to retrieve elements

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -20,6 +20,9 @@ describe('Application', () => {
     })
     expect(nameElement).toBeInTheDocument()
 
+    const nameElement2 = screen.getByLabelText('Name')
+    expect(nameElement2).toBeInTheDocument()
+
     const bioElement = screen.getByRole('textbox', {
       name: 'Bio'
     })
@@ -30,6 +33,9 @@ describe('Application', () => {
 
     const termsElement = screen.getByRole('checkbox')
     expect(termsElement).toBeInTheDocument()
+
+    const termsElement2 = screen.getByLabelText('I agree to the terms and conditions')
+    expect(termsElement2).toBeInTheDocument()
 
     const submitButtonElement = screen.getByRole('button')
     expect(submitButtonElement).toBeInTheDocument()
