9ddaf6cdedf4e1be897156b2e07e0cec05402674
Dan Carlier <dan.carlier@gmail.com>
2023-06-22 20:59:23 +1000

    Add test to check that there is a list item for each item in the skills array prop

--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -9,4 +9,10 @@ describe('Skills', () => {
     const listElement = screen.getByRole('list')
     expect(listElement).toBeInTheDocument()
   })
+
+  test('renders a list of skills', () => {
+    render(<Skills skills={skills}/>)
+    const listItemElements = screen.getAllByRole('listitem')
+    expect(listItemElements).toHaveLength(skills.length)
+  })
 })
